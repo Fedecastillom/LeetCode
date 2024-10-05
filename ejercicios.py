317f55bab70069e1604fc5d01acc8319657c4ba2
Fedecastillom <fmascm20@gmail.com>
2024-10-04 22:00:56 -0300

    majority, threesum (pointers)

--- ejercicios.py
+++ ejercicios.py
@@ -190,3 +190,48 @@ def mergeTwoLists(list1, list2):
     list1.sort()
     return list1
 
+# 16 (Pointers)
+
+def threeSum(nums):
+    res = []
+    nums.sort()
+
+    for i in range(len(nums)):
+        if i > 0 and nums[i] == nums[i-1]:
+            continue
+        
+        j = i + 1
+        k = len(nums) - 1
+
+        while j < k:
+            total = nums[i] + nums[j] + nums[k]
+
+            if total > 0:
+                k -= 1
+            elif total < 0:
+                j += 1
+            else:
+                res.append([nums[i], nums[j], nums[k]])
+                j += 1
+
+                while nums[j] == nums[j-1] and j < k:
+                    j += 1
+    
+    return res
+
+# 17
+
+def majorityElement(nums):
+    d = {}
+    ans = None
+    count = 0
+    for x in nums:
+        if x in d:
+            d[x] +=1
+        else:
+            d[x] = 1
+    for x, y in d.items():
+        if y > count:
+            count = y
+            ans = x
+    return ans
